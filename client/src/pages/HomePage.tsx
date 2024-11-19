045c21fff9867e8eb86905767357981ea1c1159d
Parth Kansara <parth.kansara@stonybrook.edu>
2024-11-18 19:21:16 -0500

    Fetching user's access tokens

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,4 +1,20 @@
+import { useLocation } from 'react-router-dom';
+
 const HomePage = () => {
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.hash.substring(1));
+    const accessToken = queryParams.get('access_token');
+    const dataAccessExpirationTime = queryParams.get('data_access_expiration_time');
+    const expiresIn = queryParams.get('expires_in');
+    const longLivedToken = queryParams.get('long_lived_token');
+
+    console.log({
+        accessToken,
+        dataAccessExpirationTime,
+        expiresIn,
+        longLivedToken
+    });
+    
     return(<></>)
 }
 
